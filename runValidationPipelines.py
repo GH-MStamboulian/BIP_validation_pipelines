66fb9956885e79a5487be3f70c41f99a383ae8b2
Moses Stamboulian <mstamboulian@hpcana03.cm.cluster>
2020-08-03 12:04:16 -0700

    changed folder name

--- runValidationPipelines.py
+++ runValidationPipelines.py
@@ -48,8 +48,11 @@ if not os.path.isdir(args._data_tables_dir):
 
 abs_dir = subprocess.Popen(["pwd"], stdout=subprocess.PIPE, shell=True).communicate()[0].decode('utf-8').replace('\n','/')
 
-if not os.path.isdir(args.output_dir):
-    os.mkdir(output_dir)
+out_dir = args.output_dir
+data_dir = args._data_tables_dir
+
+if not os.path.isdir(out_dir):
+    os.mkdir(out_dir)
 
 bip_out_dir = "/ghds/ivd/flowcentral/"
 
@@ -69,9 +72,9 @@ def get_version(version):
     
 version1, version2 = get_version(version1), get_version(version2)
 
-####################
-##  compare accuracy
-####################
+print('####################')
+print('##  compare accuracy')
+print('####################')
 
 accuracy_data_dir = data_dir + "accuracy/"
 accuracy_out_dir = out_dir + "accuracy_comparsion/"
@@ -79,12 +82,13 @@ accuracy_out_dir = out_dir + "accuracy_comparsion/"
 if not os.path.isdir(accuracy_out_dir):
     os.mkdir(accuracy_out_dir)
 
-os.system("python3 BIP_validation_pipelines_accuracy/compare_accuracy_values.py -o " + accuracy_out_dir + " -d1 " + accuracy_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + accuracy_data_dir + version2+"/" + " -v2 " + version2)
+#os.system("python3 BIP_validation_pipelines_accuracy/compare_accuracy_values.py -o " + accuracy_out_dir + " -d1 " + accuracy_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + accuracy_data_dir + version2+"/" + " -v2 " + version2)
+print("python3 BIP_validation_pipelines_accuracy/compare_accuracy_values.py -o " + accuracy_out_dir + " -d1 " + accuracy_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + accuracy_data_dir + version2+"/" + " -v2 " + version2)
 
 
-#####################
-##  compare precision
-#####################
+print('#####################')
+print('##  compare precision')
+print('#####################')
 
 precision_data_dir = data_dir + "precision/"
 precision_out_dir = out_dir + "precision_comparsion/"
@@ -92,12 +96,13 @@ precision_out_dir = out_dir + "precision_comparsion/"
 if not os.path.isdir(precision_out_dir):
     os.mkdir(precision_out_dir)
 
-os.system("python3 BIP_validation_pipelines_precision/compare_precision_values.py -o " + precision_out_dir + " -d1 " + precision_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + precision_data_dir + version2+"/" + " -v2 " + version2)
+#os.system("python3 BIP_validation_pipelines_precision/compare_precision_values.py -o " + precision_out_dir + " -d1 " + precision_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + precision_data_dir + version2+"/" + " -v2 " + version2)
+print("python3 BIP_validation_pipelines_precision/compare_precision_values.py -o " + precision_out_dir + " -d1 " + precision_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + precision_data_dir + version2+"/" + " -v2 " + version2)
 
 
-#######################
-##  compare sensitivity
-#######################
+print('#######################')
+print('##  compare sensitivity')
+print('#######################')
 
 sensitivity_data_dir = data_dir + "sensitivity/"
 sensitivity_out_dir = out_dir + "sensitivity_comparsion/"
@@ -105,12 +110,13 @@ sensitivity_out_dir = out_dir + "sensitivity_comparsion/"
 if not os.path.isdir(sensitivity_out_dir):
     os.mkdir(sensitivity_out_dir)
 
-os.system("python3 BIP_validation_pipelines_LoD/compare_LoD_values.py -i1 " + bip_out_dir + " -i2 " + bip_out_dir + " -o " + sensitivity_out_dir + " -d1 " + sensitivity_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + sensitivity_data_dir + version2+"/" + " -v2 " + version2)
+#os.system("python3 BIP_validation_pipelines_LoD/compare_LoD_values.py -i1 " + bip_out_dir + " -i2 " + bip_out_dir + " -o " + sensitivity_out_dir + " -d1 " + sensitivity_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + sensitivity_data_dir + version2+"/" + " -v2 " + version2)
+print("python3 BIP_validation_pipelines_LoD/compare_LoD_values.py -i1 " + bip_out_dir + " -i2 " + bip_out_dir + " -o " + sensitivity_out_dir + " -d1 " + sensitivity_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + sensitivity_data_dir + version2+"/" + " -v2 " + version2)
 
 
-#######################
-##  compare specificity
-#######################
+print('#######################')
+print('##  compare specificity')
+print('#######################')
 
 specificity_data_dir = data_dir + "specificity/"
 specificity_out_dir = out_dir + "specificity_comparsion/"
@@ -118,4 +124,5 @@ specificity_out_dir = out_dir + "specificity_comparsion/"
 if not os.path.isdir(specificity_out_dir):
     os.mkdir(specificity_out_dir)
 
-os.system("python3 BIP_validation_pipelines_LoB/compare_LoB_values.py -i1 " + bip_out_dir + " -i2 " + bip_out_dir + " -o " + specificity_out_dir + " -d1 " + specificity_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + specificity_data_dir + version2+"/" + " -v2 " + version2)
+#os.system("python3 BIP_validation_pipelines_LoB/compare_LoB_values.py -i1 " + bip_out_dir + " -i2 " + bip_out_dir + " -o " + specificity_out_dir + " -d1 " + specificity_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + specificity_data_dir + version2+"/" + " -v2 " + version2)
+print("python3 BIP_validation_pipelines_LoB/compare_LoB_values.py -i1 " + bip_out_dir + " -i2 " + bip_out_dir + " -o " + specificity_out_dir + " -d1 " + specificity_data_dir + version1+"/" + " -v1 " + version1 + " -d2 " + specificity_data_dir + version2+"/" + " -v2 " + version2)
